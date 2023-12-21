03588f7ddfc2ced4b6288cda9b0f2aa4f845525b
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-21 23:28:35 +0100

    Prepare home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { user } = useSelector((state) => state.user);
+
+  return (
+    <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden"></div>
+  );
 };
 
 export default Home;
