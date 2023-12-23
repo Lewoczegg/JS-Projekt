35bdbab3a00135dc76f8e57a6642a7868214a89f
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-23 13:28:10 +0100

    Add Friend Request UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { FriendsCard, ProfileCard, TopBar } from "../components";
+import { CustomButton, FriendsCard, ProfileCard, TopBar } from "../components";
+import { friends, requests } from "../assets/data";
+import { Link } from "react-router-dom";
+import { NoProfile } from "../assets";
 
 const Home = () => {
   const { user } = useSelector((state) => state.user);
+  const [friendRequest, setFriendRequest] = useState(requests);
+  const [suggestedFriends, setSuggestedFriends] = useState(requests);
 
   return (
     <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-full overflow-hidden">
@@ -15,10 +20,58 @@ const Home = () => {
           <ProfileCard user={user} />
           <FriendsCard friends={user?.friends} />
         </div>
+
         {/* CENETR */}
-        <div></div>
+        <div className="flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto"></div>
+
         {/* RIGHT */}
-        <div></div>
+        <div className="hidden w-1/4 h-full lg:flex flex-col gap-8 overfolw-y-auto">
+          {/* FREIND REQUEST */}
+          <div className="w-full bg-primary shadow-sm rounded-lg px-6 py-5">
+            <div className="flex items-center justify-between text-xl text-ascent-1 pb-2 border-b border-[#66666645]">
+              <span>Friend Request</span>
+              <span>{friendRequest?.length}</span>
+            </div>
+            <div className="w-full flex flex-col gap-4 pt-4">
+              {friendRequest?.map(({ _id, requestFrom: from }) => (
+                <div key={_id} className="flex items-center justify-between">
+                  <Link
+                    to={"/profile/" + from?._id}
+                    className="w-full flex gap-4 items-center curosr-pointer"
+                  >
+                    <img
+                      src={from?.profileUrl ?? NoProfile}
+                      alt={from?.firstName}
+                      className="w-10 h-10 object-cover rounded-full"
+                    />
+                    <div className="flex-1 ">
+                      <p className="text-base font-medium text-ascent-1">
+                        {from?.firstName} {from?.lastName}
+                      </p>
+                      <span className="text-sm text-ascent-2">
+                        {from?.profession ?? "No Profession"}
+                      </span>
+                    </div>
+                  </Link>
+
+                  <div className="flex gap-1">
+                    <CustomButton
+                      title="Accept"
+                      containerStyles="bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full"
+                    />
+                    <CustomButton
+                      title="Deny"
+                      containerStyles="border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* SUGGESTED FRIENDS */}
+          <div></div>
+        </div>
       </div>
     </div>
   );
