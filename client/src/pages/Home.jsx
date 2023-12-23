dc821e7a7cf19c9da6acc06ae91f4f2a684de641
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-23 12:52:36 +0100

    Add Friends Card UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { ProfileCard, TopBar } from "../components";
+import { FriendsCard, ProfileCard, TopBar } from "../components";
 
 const Home = () => {
   const { user } = useSelector((state) => state.user);
 
   return (
-    <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
+    <div className="w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-full overflow-hidden">
       <TopBar />
 
       <div className="w-full flex gap-2 lg:gap4 pt-5 pb-10 h-full">
         {/* LEFT */}
         <div className="hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overfolw-y-auto">
           <ProfileCard user={user} />
+          <FriendsCard friends={user?.friends} />
         </div>
         {/* CENETR */}
         <div></div>
