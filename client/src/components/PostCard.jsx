4b1c450c54bbb2f2d849d2d55b805afd6ab8e26c
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-27 17:29:35 +0100

    Add comment form UI

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -4,6 +4,75 @@ import { NoProfile } from "../assets";
 import moment from "moment";
 import { BiComment, BiLike, BiSolidLike } from "react-icons/bi";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+import { useForm } from "react-hook-form";
+import TextInput from "./TextInput";
+import CustomButton from "./CustomButton";
+import Loading from "./Loading";
+
+const CommentForm = ({ user, id, replyAt, getComments }) => {
+  const [loading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data) => {};
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full border-b border-[#66666645]"
+    >
+      <div className="w-full flex items-center gap-2 py-4">
+        <img
+          src={user?.profileUrl ?? NoProfile}
+          alt="User Image"
+          className="w-10 h-10 rounded-full object-cover"
+        />
+
+        <TextInput
+          name="comment"
+          styles="w-full rounded-full py-3"
+          placeholder={replyAt ? `Reply @${replyAt}` : "Comment this post"}
+          register={register("comment", {
+            required: "Comment can not be empty",
+          })}
+          error={errors.comment ? errors.comment.message : ""}
+        />
+      </div>
+
+      {errMsg?.message && (
+        <span
+          role="alert"
+          className={`text-sm ${
+            errMsg?.status === "failed"
+              ? "text-[#f64949fe]"
+              : "text-[#2ba150fe]"
+          }`}
+        >
+          {errMsg?.message}
+        </span>
+      )}
+
+      <div className="flex items-end justify-end pb-2">
+        {loading ? (
+          <Loading />
+        ) : (
+          <CustomButton
+            title="Submit"
+            type="submit"
+            containerStyles="bg-[#0444a4] text-white py-1 px-3 rounded-full font-semibold text-sm"
+          />
+        )}
+      </div>
+    </form>
+  );
+};
 
 const PostCard = ({ post, user, deletePost, likePost }) => {
   const [showAll, setShowAll] = useState(0);
@@ -106,6 +175,17 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
           </div>
         )}
       </div>
+
+      {/* COMMENTS */}
+      {showComments === post?._id && (
+        <div className="w-full mt-4 border-t border-[#66666645] pt-4">
+          <CommentForm
+            user={user}
+            id={post?._id}
+            getComments={() => getComments(post?._id)}
+          />
+        </div>
+      )}
     </div>
   );
 };
