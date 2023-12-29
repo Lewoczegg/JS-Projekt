9952712d0e825f566bb2643667b8f9cf6008b14a
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-29 18:00:35 +0100

    Add email verification route and view

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,3 +6,9 @@ const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
 
 router.get("/verify/:userId/:token", verifyEmail);
+
+router.get("/verified", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+});
+
+export default router;
