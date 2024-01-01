16c07d77d01fdc1bb3c0687269f496cbe8a0767e
Lewoczegg <mateusz.lewocz46@gmail.com>
2024-01-01 17:36:55 +0100

    Add user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,13 @@ import express from "express";
 import path from "path";
 import {
   changePassword,
+  getUser,
   requestPasswordReset,
   resetPassword,
+  updateUser,
   verifyEmail,
 } from "../controllers/userController.js";
+import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
@@ -16,6 +19,10 @@ router.get("/verify/:userId/:token", verifyEmail);
 router.post("/request-passwordreset", requestPasswordReset);
 router.get("/reset-password/:userId/:token", resetPassword);
 router.post("/reset-password", changePassword);
+
+// USER ROUTES
+router.post("/get-user/:id?", userAuth, getUser);
+router.put("/update-user", userAuth, updateUser);
 
 router.get("/verified", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/build", "index.html"));
