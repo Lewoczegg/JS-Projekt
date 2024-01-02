97e0991e83fc2b8e88f061f1571857949b69db8e
Lewoczegg <mateusz.lewocz46@gmail.com>
2024-01-02 17:02:34 +0100

    Add like and comment on posts methods

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import userAuth from "../middleware/authMiddleware.js";
 import {
+  commentPost,
   createPost,
   getComments,
   getPost,
   getPosts,
   getUserPosts,
+  likePost,
+  likePostComment,
+  replyPostComment,
 } from "../controllers/postController.js";
 
 const router = express.Router();
@@ -20,5 +24,11 @@ router.get("/get-user-post/:id", userAuth, getUserPosts);
 
 // GET COMMENTS
 router.get("/comments/:postId", getComments);
+
+// LIKE AND COMMENT ON POSTS
+router.post("/like/:id", userAuth, likePost);
+router.post("/like-comment/:id/:rid?", userAuth, likePostComment);
+router.post("/comment/:id", userAuth, commentPost);
+router.post("/reply-comment/:id", userAuth, replyPostComment);
 
 export default router;
