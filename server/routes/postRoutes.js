447b0f84d3ec1aa44e8893a1cc9fb6b9866cc69d
Lewoczegg <mateusz.lewocz46@gmail.com>
2024-01-02 16:20:28 +0100

    Add getPosts method

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import userAuth from "../middleware/authMiddleware.js";
-import { createPost } from "../controllers/postController.js";
+import { createPost, getPosts } from "../controllers/postController.js";
 
 const router = express.Router();
 
 // CREATE POST
 router.post("/create-post", userAuth, createPost);
+
+// get posts
+router.post("/", userAuth, getPosts);
 
 export default router;
