f1dde2f7fd64b7fd1b2e0bbae47f7887404ccca6
Lewoczegg <mateusz.lewocz46@gmail.com>
2024-01-04 13:08:12 +0100

    Connect frontend comments with backend API

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -8,7 +8,20 @@ import { useForm } from "react-hook-form";
 import TextInput from "./TextInput";
 import CustomButton from "./CustomButton";
 import Loading from "./Loading";
-import { postComments } from "../assets/data";
+import { apiRequest } from "../utils";
+
+const getPostComments = async (id) => {
+  try {
+    const res = await apiRequest({
+      url: "/posts/comments/" + id,
+      method: "GET",
+    });
+
+    return res?.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const ReplyCard = ({ reply, user, handleLike }) => {
   return (
@@ -42,7 +55,7 @@ const ReplyCard = ({ reply, user, handleLike }) => {
             onClick={handleLike}
           >
             {reply?.likes?.includes(user?._id) ? (
-              <BiSolidLike size={20} color="blie" />
+              <BiSolidLike size={20} color="blue" />
             ) : (
               <BiLike size={20} />
             )}
@@ -66,7 +79,42 @@ const CommentForm = ({ user, id, replyAt, getComments }) => {
     mode: "onChange",
   });
 
-  const onSubmit = async (data) => {};
+  const onSubmit = async (data) => {
+    setLoading(true);
+    setErrMsg("");
+    try {
+      const URL = !replyAt
+        ? "/posts/comment/" + id
+        : "/posts/reply-comment/" + id;
+
+      const newData = {
+        comment: data?.comment,
+        from: user?.firstName + " " + user.lastName,
+        replyAt: replyAt,
+      };
+
+      const res = await apiRequest({
+        url: URL,
+        data: newData,
+        token: user?.token,
+        method: "POST",
+      });
+
+      if (res?.status === "failed") {
+        setErrMsg(res);
+      } else {
+        reset({
+          comment: "",
+        });
+        setErrMsg("");
+        await getComments();
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
   return (
     <form
@@ -127,10 +175,11 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
   const [replyComments, setReplyComments] = useState(0);
   const [showComments, setShowComments] = useState(0);
 
-  const getComments = async () => {
+  const getComments = async (id) => {
     setReplyComments(0);
+    const result = await getPostComments(id);
 
-    setComments(postComments);
+    setComments(result);
     setLoading(false);
   };
 
@@ -272,7 +321,12 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
                   <p className="text-ascent-2">{comment?.comment}</p>
 
                   <div className="mt-2 flex gap-6">
-                    <p className="flex gap-2 items-center text-base text-ascent-2 cursor-pointer">
+                    <p
+                      className="flex gap-2 items-center text-base text-ascent-2 cursor-pointer"
+                      onClick={() => {
+                        handleLike("/posts/like-comment/" + comment?._id);
+                      }}
+                    >
                       {comment?.likes?.includes(user?._id) ? (
                         <BiSolidLike size={20} color="blue" />
                       ) : (
@@ -323,7 +377,7 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
                         key={reply?._id}
                         handleLike={() =>
                           handleLike(
-                            "/posts/like-comment" +
+                            "/posts/like-comment/" +
                               comment?._id +
                               "/" +
                               reply?._id
